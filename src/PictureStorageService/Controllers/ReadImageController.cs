fda41dbfdf2e96f2880cbd9edb80f15f8e9863ef
VillSource <anirut1800@gmail.com>
2023-07-16 21:26:42 +0700

    add image endpoint

---
+++ src/PictureStorageService/Controllers/ReadImageController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace PictureStorageService.Controllers;
+
+[Route("img")]
+[ApiController]
+public class ReadImageController : ControllerBase
+{
+    [HttpGet("{img-id}")]
+    public IActionResult GetImgById([FromRoute(Name ="img-id")] string id)
+    {
+
+        string path = 
+            $""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+             /img-storage/{id}
+             """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""";
+
+        FileStream fsSource = new(path, FileMode.Open, FileAccess.Read);
+
+        return File(fsSource, Image.Jpeg);
+    }
+}
